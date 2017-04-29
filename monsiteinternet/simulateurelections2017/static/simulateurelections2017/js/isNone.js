7af2e7cfa46b8e9bd0ae3ee521d6c4134e7eca74
theo vast <tvast>
2017-04-29 17:46:17 +0200

    change result view

--- monsiteinternet/simulateurelections2017/static/simulateurelections2017/js/isNone.js
+++ monsiteinternet/simulateurelections2017/static/simulateurelections2017/js/isNone.js
@@ -1,18 +1,14 @@
    function toggleDisplay() {
-        console.log ('coucou')
-        var x = document.getElementsByClassName('isNone');
-        if (x.style.display === 'none') {
-            x.style.display = 'block';
-        } else {
-            x.style.display = 'none';
-        }
+        // var x = document.getElementsByClassName('isNone');
+        // console.log(x);
+        // if (x.style.display === 'none') {
+        //     x.style.display = 'block';
+        // } else {
+        //     x.style.display = 'none';
+        // }
+        var x = document.getElementById("mp"); // Using a class instead, see note below.
+        x.classList.toggle('isNone');
+        var x = document.getElementById("em"); // Using a class instead, see note below.
+        x.classList.toggle('isNone');
     }
 
-    function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
